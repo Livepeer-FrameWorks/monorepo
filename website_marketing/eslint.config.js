f290bd572a3273ed7a7f1f84e3f7148ad42581ec
Marco van Dijk <marco@stronk.rocks>
2026-02-25 13:55:50 +0100

    drop `eslint-plugin-react` until they update for eslint 10

--- website_marketing/eslint.config.js
+++ website_marketing/eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import jsxA11y from "eslint-plugin-jsx-a11y";
@@ -22,26 +21,15 @@ export default [
         ...globals.node,
       },
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
     plugins: {
-      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "jsx-a11y": jsxA11y,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...jsxA11y.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-      "react/no-unescaped-entities": "off",
-      "react/prop-types": "off",
       "react-refresh/only-export-components": "off",
       "prefer-const": "error",
       "no-var": "error",
